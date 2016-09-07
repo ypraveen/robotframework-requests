6e7dfa44fdde3cb90a405b91e48c50b0d62ce390
Praveen Yalagandula <ypraveen@avinetworks.com>
2016-09-07 22:57:53 +0000

    Removing execfile call from setup.py to make it useable with python3

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@
 from distutils.core import setup
 
 from os.path import abspath, dirname, join
-execfile(join(dirname(abspath(__file__)), 'src', 'RequestsLibrary', 'version.py'))
+
+version_fname = join(dirname(abspath(__file__)), 'src', 'RequestsLibrary', 'version.py')
+with open(version_fname, "r") as f:
+    code = compile(f.read(), version_fname, 'exec')
+    exec(code)
 
 DESCRIPTION = """
 Robot Framework keyword library wrapper around the HTTP client library requests.
